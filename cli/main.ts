7bbda06fb2a4c80b8f958aca43ac8fea32a391aa
Jan Gabriel H. Que <jan.que@whiterain-dti.com>
2024-11-06 11:49:18 +0800

    release patch fixes

--- cli/main.ts
+++ cli/main.ts
@@ -3,7 +3,7 @@ import { cliRunner } from "./task/index.ts"
 
 await new Command()
   .name("modo")
-  .version("0.1.7")
+  .version("0.2.0")
   .description("Test CLI for monorepos on deno")
   .arguments("<args>")
   .option("-c, --clear", "Clear console on execute")
